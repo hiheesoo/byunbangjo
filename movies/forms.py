fb4e72dbf99205248074c72bb484e1c4c78722f2
nansae0804 <nansae0804@gmail.com>
2024-10-18 17:10:58 +0900

    앱 forms.py

---
+++ movies/forms.py
@@ -0,0 +1,12 @@
+from django import forms 
+from .models import Movie, Comment
+
+class MovieForm(forms.ModelForm):
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+class CommentForm(forms.Form):
+    class Meta:
+        model = Comment
+        fields = '__all__'
