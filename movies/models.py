b14484573a9652611db01205e8614c4f32c2edbe
sungjoon <happypigs7@gmail.com>
2024-10-18 16:57:36 +0900

    migrations

--- movies/models.py
+++ movies/models.py
@@ -1,18 +1,18 @@
 from django.db import models
+from django.conf import settings
 # from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    b_author = models.TextField()
-    like = models.TextField()
-    dislike = models.TextField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_users')
+    dislike_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="dislike_users")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     comment = models.TextField()
-    c_author = models.TextField()
-    movieinfos = models.TextField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
