eca0d83902b9284811d8441ba7f3e86ddb021e9d
sungjoon <happypigs7.dev@gmail.com>
2024-10-20 17:50:51 +0900

    디버깅, 메인페이지 기능구현 완료

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # byunbangjo
 
-ddd
-
-## test
+## 수정사항
+- 10/20 방성준
+    - 영화 create, detail 기능 구현 및 디버깅
+    - index(main page)
+        - DB에 영화 있을 시 
+            - 있는 영화 전부 출력
+        - DB에 영화 없을 시
+            - 생성 버튼(create)
+        - 좋아요, 싫어요 기능 구현
--- movies/urls.py
+++ movies/urls.py
@@ -5,4 +5,7 @@ app_name = 'movies'
 urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.create, name='create'),
+    path('detail/<int:movie_pk>', views.detail, name='detail'),
+    path('likes/<int:movie_pk>/', views.likes, name='likes'),
+    path('dislikes/<int:movie_pk>/', views.dislikes, name='dislikes'),
 ]
